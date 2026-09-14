9415b5f0bb46de9c0bc2abb290b93d0b0cd5c36e
Gopika-Ramesh27 <gopiramesh2023@gmail.com>
2026-09-14 15:24:50 +0530

    Refactor MySQL connection to use environment variable
    Updated MySQL connection string to use environment variable for password.

--- earthquake_mysql.py
+++ earthquake_mysql.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -5,7 +6,7 @@ df = pd.read_csv("cleaned_earthquake_data.csv")
 df["time"] = pd.to_datetime(df["time"],errors="coerce")
 df["updated"] = pd.to_datetime(df["updated"],errors="coerce")
 print(df.shape)
-engine = create_engine("mysql+pymysql://root:gopi_ramesh@localhost/project_db")
+engine = create_engine(f"mysql+pymysql://root:{os.getenv('MYSQL_PASSWORD')}@localhost/project_db")
 with engine.connect()as conn:
     print("MySql connected successfully!")
 df.to_sql("earthquakes", con=engine, if_exists="replace", index=False)
